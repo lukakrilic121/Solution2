84d6722abdbc85483dfba2f903ca2b98b5ee3b60
lukakrilic96 <lukakrilic96@gmail.com>
2017-10-05 21:31:34 +0200

    dodali smo VB.NET project

---
+++ csharp_consola/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace csharp_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow world");
+            Console.ReadKey();
+        }
+    }
+}
