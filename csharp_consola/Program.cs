a947e90692d939b5e4adac3ce9618df16c452142
lukakrilic96 <lukakrilic96@gmail.com>
2017-10-12 19:31:03 +0200

    dodali smo for petlju

--- csharp_consola/Program.cs
+++ csharp_consola/Program.cs
@@ -7,6 +7,12 @@ namespace csharp_consola
     {
         static void Main(string[] args)
         {
+            int sum=0;
+            for(int i = 0; i < 100; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
             Console.WriteLine("Hellow world");
             Console.ReadKey();
         }
